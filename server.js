3623dcbec558c4f65379e5f9237e021a5eb1143c
Slanish <slanish1996@gmail.com>
2017-02-13 23:45:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,58 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
+var artical1 = {
+    title : 'artical',
+    heading :'art',
+    content :`<div>
+            <p>
+                balta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blata
+            </p>
+            <p>
+                balta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blata
+            </p>
+            <p>
+                balta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blatabalta blata
+            </p>
+        </div>
+        </div>
+        `
+        
+    
+    
+};
+function createtemplate (data){
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
+var htmltemplate=`
+    <html>
+    <head>
+    <title>${title}</title>
+ <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">home</a>
+        </div>
+        <hr/>
+        <h3>${heading}</h3>
+        <div>date</div>
+      <div>
+      ${content}
+        </div>
+    </body>
+</html>
+`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/artical1.html',function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'artical1.html'));
+    res.send(createtemplate(artical1));
 });
 
 app.get('/ui/style.css', function (req, res) {
