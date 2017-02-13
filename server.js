9272ce5b8211d132aba5526971f2dfcd93721d45
Slanish <slanish1996@gmail.com>
2017-02-13 22:15:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/artical1',function(req,res) {
+app.get('/artical1.html',function(req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'artical1.html'));
 });
 
